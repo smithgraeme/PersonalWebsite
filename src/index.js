0abf990f3a8fc0ca75158609894a7d57b6c9cbe5
Graeme Smith <graeme.s@outlook.com>
2018-11-22 11:19:33 -0700

    test deployment pipeline

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import Dashboard from './Dashboard';
 
 ReactDOM.render(<Dashboard />, document.getElementById('root'));
